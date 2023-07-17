30946483471e281ec68610caac23a4beb11c519c
pedropeglow <pedropeglowm@gmail.com>
2023-07-17 17:29:05 -0300

    :sparkles: Interagindo com dados da WEB.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById('lista')
-const itens = []
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+} )
 
 
 form.addEventListener("submit", (evento) => {
@@ -8,32 +12,30 @@ form.addEventListener("submit", (evento) => {
     
     const nome = evento.target.elements['nome'].value
     const quantidade = evento.target.elements['quantidade'].value
-    
-    criaElemento(nome, quantidade);
-    evento.target.elements['nome'] = ""
-    evento.target.elements['quantidade'] = ""
-})
-
-function criaElemento(nome, quantidade) {
-    console.log(nome)
-    console.log(quantidade)
-
-    const novoItem = document.createElement('li')
-    novoItem.classList.add("item")
-
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
-
-    novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
-    lista.appendChild(novoItem)
 
     const itemAtual = {
         "nome": nome,
         "quantidade": quantidade
     }
+    
+    criaElemento(itemAtual);
 
     itens.push(itemAtual)
 
-    localStorage.setItem("item", JSON.stringify(itens))
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    evento.target.elements['nome'] = ""
+    evento.target.elements['quantidade'] = ""
+})
+
+function criaElemento(item) {
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = item.quantidade
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += item.nome
+    lista.appendChild(novoItem)
 }
