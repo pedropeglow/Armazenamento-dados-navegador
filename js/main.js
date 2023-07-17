85ededa4e18719578272ae4668a95ed6ffd3b9fe
pedropeglow <pedropeglowm@gmail.com>
2023-07-17 16:18:49 -0300

    :tada: Armazenando dados no navegador

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("novoItem")
+const lista = document.getElementById('lista')
+const itens = []
+
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+    
+    const nome = evento.target.elements['nome'].value
+    const quantidade = evento.target.elements['quantidade'].value
+    
+    criaElemento(nome, quantidade);
+    evento.target.elements['nome'] = ""
+    evento.target.elements['quantidade'] = ""
+})
+
+function criaElemento(nome, quantidade) {
+    console.log(nome)
+    console.log(quantidade)
+
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+    lista.appendChild(novoItem)
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("item", JSON.stringify(itens))
+}
